8ed477d402eba9f6ccffd4d8dc15642b164f55b0
shohan <shohan.ece17@gmail.com>
2024-08-28 23:29:09 +0600

    start slicing

--- video-website/src/App.js
+++ video-website/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Home from './pages/Home';
-import Video from './pages/Video';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
 
 function App() {
-  return (
-    <>
-    <Home />
-    <Video />
-    </>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/videos/:videoId" element={<Video />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
--- video-website/src/pages/Video.js
+++ video-website/src/pages/Video.js
@@ -1,14 +1,10 @@
 import VideoPlayer from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
-import Footer from "../components/Footer";
 import RelatedVideoList from "../components/list/RelatedVideoList";
-import Navbar from "../components/navbar/Navbar";
 
 export default function Video() {
     return (
         <>
-            <Navbar />
-
             <section class="pt-6 pb-20">
                 <div class="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
                     <div class="grid grid-cols-3 gap-2 lg:gap-8">
@@ -23,7 +19,6 @@ export default function Video() {
                 </div>
             </section>
 
-            <Footer />
         </>
     );
 }
