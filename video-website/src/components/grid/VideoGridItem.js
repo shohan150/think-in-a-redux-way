9113519308251960f5e718567e074d5ab2273558
shohan <shohan.ece17@gmail.com>
2024-08-31 12:14:16 +0600

    homepage setup

--- video-website/src/components/grid/VideoGridItem.js
+++ video-website/src/components/grid/VideoGridItem.js
@@ -1,46 +1,48 @@
 import { Link } from "react-router-dom";
 
-export default function VideoGridItem() {
+export default function VideoGridItem({video}) {
+    console.log(video);
+    
     return (
         <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
             <div className="w-full flex flex-col">
                 <div className="relative">
-                    <Link to="/videos/1">
+                    <Link to={`videos/${video.id}`}>
                         <img
-                            src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+                            src={video.thumbnail}
                             className="w-full h-auto"
-                            alt="Some video title"
+                            alt={video.title}
                         />
                     </Link>
 
                     <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-                        12:10
+                        {video.duration}
                     </p>
                 </div>
 
                 <div className="flex flex-row mt-2 gap-2">
-                    <Link to="/videos/1" className="shrink-0">
+                    <Link to={`videos/${video.id}`} className="shrink-0">
                         <img
-                            src="https://avatars.githubusercontent.com/u/73503432?v=4"
+                            src={video.avatar}
                             className="rounded-full h-6 w-6"
                             alt="Learn with Sumit"
                         />
                     </Link>
 
                     <div clas="flex flex-col">
-                        <Link to="/videos/1">
+                        <Link to={`videos/${video.id}`}>
                             <p className="text-slate-900 text-sm font-semibold">
-                                Video title
+                               {video.title}
                             </p>
                         </Link>
                         <Link
                             className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-                            to="/videos/1"
+                            to={`videos/${video.id}`}
                         >
-                            Learn with Sumit
+                            {video.author}
                         </Link>
                         <p className="text-gray-400 text-xs mt-1">
-                            200 views . May 3, 2022
+                            {video.views} views . {video.date}
                         </p>
                     </div>
                 </div>
--- video-website/src/features/videos/videosSlice.js
+++ video-website/src/features/videos/videosSlice.js
@@ -2,6 +2,7 @@ import { getVideos } from "./videosAPI";
 
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
+//declare the initialState with video, loading and error data.
 const initialState = {
     videos: [],
     isLoading: false,
@@ -9,26 +10,31 @@ const initialState = {
     error: "",
 };
 
-// async thunk
+// async thunk. fetch video data asynchronously from json server using createAsyncThunk. the 2nd parameter takes an async callback that returns a promise. inside it the promise is made or requested on the getVideos function. depending on what this function returns the result of the promise depends.if successfull return the data with the promise fulfill.
 export const fetchVideos = createAsyncThunk("videos/fetchVideos", async () => {
     const videos = await getVideos();
     return videos;
 });
 
+//create the videoSlice
 const videoSlice = createSlice({
     name: "videos",
     initialState,
+    // directly ei reducer er kono kaj nai. etar kaj sudhu data fetch kora. sejonno e extraReducer e lagbe sudhu. sekhane 3 type promise er jonno builder format e 3 ta case or situation er jonno callback declare kori.
     extraReducers: (builder) => {
         builder
             .addCase(fetchVideos.pending, (state) => {
+                //request ongoing. loading true. error none.
                 state.isError = false;
                 state.isLoading = true;
             })
             .addCase(fetchVideos.fulfilled, (state, action) => {
+                //data successfully received. loading complete. keep the data from received payload inside state.
                 state.isLoading = false;
                 state.videos = action.payload;
             })
             .addCase(fetchVideos.rejected, (state, action) => {
+                //error occured. loading phase complete, resulting into failure. no data received, set the error from received action.
                 state.isLoading = false;
                 state.videos = [];
                 state.isError = true;
@@ -37,4 +43,7 @@ const videoSlice = createSlice({
     },
 });
 
+//finally export the reducer. 
 export default videoSlice.reducer;
+
+//sathe fetchvideos const tao agei export kora ache. shei const k dispatch korle e videoSlice er extraReducer theke execute hobe. tobe thunk e const asole action creator er moto kaj kore. mane shei const k invoke korle, fetchVideos(), execute hoye server e request kore actionType/actionCreator toiri kore, shei creator er vitor action type, payload diye dispatch k dibe. tokhon asole shei action areator ta dispatch holo ebong videoSlice e ese extraReducer e ese execute hobe. 
